7ec0546ea94f6d5f366d158c910d8225ed1716ba
Agregat <malushewtosha@list.ru>
2025-06-14 14:34:06 +0300

    ага

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import csv
+
 from get_names import read_names
 from get_initials import format_names
 from write_names import write_names
@@ -7,7 +9,13 @@ import config
 def main():
     names = read_names(config.READ_PATH)
     formated_names = format_names(names)
-    write_names(formated_names, config.WRITE_PATH)
+    sorted_names = sorted(formated_names, key=lambda x: x.split()[-1])
+    csv_data = [(i + 1, name) for i, name in enumerate(sorted_names)]
+    with open(config.WRITE_PATH, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["#", "Инициалы"])
+        writer.writerows(csv_data)
+
     # Задание: писать инициалы отсортированными по алфавиту (по фамилии)
     # Дописать README.md
     # Задание*: писать порядковый номер и в формате csv
